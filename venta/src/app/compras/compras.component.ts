4e460c3737d848d52c2913437413a22e11c8b260
Alejandro Vazquez <alejandro_vazquez98@hotmail.com>
2019-10-02 07:50:44 -0500

    Compras con base ya creada, y obteniendo el valor a buscar, estructura de Inventario creada

--- venta/src/app/compras/compras.component.ts
+++ venta/src/app/compras/compras.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-compras',
-  templateUrl: './compras.component.html',
-  styleUrls: ['./compras.component.css']
+	selector: 'app-compras',
+	templateUrl: './compras.component.html',
+	styleUrls: ['./compras.component.css']
 })
 export class ComprasComponent implements OnInit {
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit() {
-  }
+	buscarProducto() {
+		var elemento = (<HTMLInputElement>document.getElementById("input--busqueda"));
+		console.log(elemento.value);
+
+		// Rutina para búsqueda de producto en Firebase.
+
+	}
+
+	ngOnInit() {
+	}
 
 }
